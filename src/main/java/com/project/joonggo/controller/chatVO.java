e2805b9479efecd97acdb9cfe999b9f216f15d4f
Umc404 <ugb0404@naver.com>
2024-12-04 11:42:14 +0900

    1.chatvo default

---
+++ src/main/java/com/project/joonggo/controller/chatVO.java
@@ -0,0 +1,14 @@
+package com.project.joonggo.controller;
+
+
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class chatVO {
+
+}
