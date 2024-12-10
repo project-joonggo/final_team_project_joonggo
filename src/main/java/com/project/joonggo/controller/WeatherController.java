01488dec65ebbaf5b39bceb5c7f2e6a5cc1ef356
Zi90 <edwin03222@gmail.com>
2024-12-10 12:39:38 +0900

    6. 수정 중

--- src/main/java/com/project/joonggo/controller/WeatherController.java
+++ src/main/java/com/project/joonggo/controller/WeatherController.java
@@ -32,9 +32,16 @@ public class WeatherController {
 
     @PostMapping("/")
     public String showWeather(@RequestParam("address") String address, Model model){
+        // 주소를 통해 위도와 경도를 얻고
         Map<String, String> lanLon = weatherService.returnLanLon(address);
+        System.out.println("Latitude and Longitude: " + lanLon);  // 이를 통해 위도/경도 값이 정상적으로 들어오는지 확인
+//        Map<String, String> weather = weatherService.returnWeather(lanLon);
+//        System.out.println("Weather data: " + weather);  // 날씨 데이터가 정상적으로 반환되는지 확인
+
 
         model.addAttribute("weather", weatherService.returnWeather(lanLon));
+        // 날씨 데이터를 모델에 담아 전달
+//        model.addAttribute("weather", weather);
         return "index";
     }
 }
