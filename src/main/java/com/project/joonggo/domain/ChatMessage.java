e84c0648350b57516872cba8aacd254247edae2d
Umc404 <ugb0404@naver.com>
2024-12-27 18:04:18 +0900

    9. chatbot final & css

---
+++ src/main/java/com/project/joonggo/domain/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.project.joonggo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    private String message;
+    private String timestamp;
+    private String content;
+    private String sender;
+}
