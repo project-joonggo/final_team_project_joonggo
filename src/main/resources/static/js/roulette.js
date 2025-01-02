9d311e812808b98c957149301f54c23d493209f9
Zi90 <edwin03222@gmail.com>
2025-01-02 14:32:55 +0900

    20. register detail css

--- src/main/resources/static/js/roulette.js
+++ src/main/resources/static/js/roulette.js
@@ -115,22 +115,3 @@ const rotate = async () => {
 
 // 룰렛 그리기
 newMake();
-
-// 모달 DOM 요소
-const customAlert = document.getElementById("custom-alert");
-const alertMessage = document.getElementById("alert-message");
-const alertOkButton = document.getElementById("alert-ok-button");
-
-// 커스터마이즈된 alert 함수
-const showAlert = (message) => {
-  alertMessage.textContent = message;
-  customAlert.classList.remove("hidden");
-  customAlert.style.display = "block";
-};
-
-// "확인" 버튼 클릭 시 모달 닫기
-alertOkButton.addEventListener("click", () => {
-  customAlert.classList.add("hidden");
-  customAlert.style.display = "none";
-});
-
