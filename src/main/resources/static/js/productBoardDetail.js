b02fc74c35e8cf56036d1f9341db40f9876d3004
dtkd19 <tkdgns9804@naver.com>
2025-01-02 18:51:29 +0900

    9. 하트색 바꿧음

--- src/main/resources/static/js/productBoardDetail.js
+++ src/main/resources/static/js/productBoardDetail.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         } else if (data.newButtonText === '찜 취소') {
                             // 찜한 경우, 채워진 하트 아이콘으로 변경
                             wishIcon.innerHTML = `
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
                                 </svg>
                             `;
